996ba0b9910ff4d11164d1d2c1a0d014460e9518
Tom Hinton <git@larkery.com>
2026-01-24 17:06:58 +0000

    logging

--- __init__.py
+++ __init__.py
@@ -131,6 +131,7 @@ def zha_expand(entities):
     for e in entities:
         if len(g2l[e]): out.update(g2l[e])
         else: out.add(e)
+        log.message(f"{e} => {g2l[e]} => {out}")
     return out
 
 @pyscript_compile
